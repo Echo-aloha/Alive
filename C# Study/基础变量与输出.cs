3a3f216184dea2ae054a50ed962276c31cfc778a
Echo-aloha <115958634+Echo-aloha@users.noreply.github.com>
2025-12-23 15:39:46 +0800

    Add basic variable and output example in C#

---
+++ C# Study/基础变量与输出.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        double b = 3.5;
+        double c = a + b;
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("a + b = " + c);
+    }
+}
